69cfa109ad9417723a3f464ba22289359fea3774
Drew S. Ortega <DrewSOrtega@gmail.com>
2019-10-05 16:36:05 -0700

    only copy list at base case, add comments to make code readable

--- permutations.py
+++ permutations.py
@@ -1,41 +1,73 @@
 import sys
 import os
-import os.path
+
+# determine whether or not each input is nummeric
 def is_int(s):
     try:
         int(s)
         return True
     except ValueError:
         return False
+
+# recursive permute function which uses N to track how many characters remaining to permute
 def permute(N, L):
+    # enpty array to store results
     result = []
     if N == 1:
-        return L
+        # if N = 1, L is our base case so return L instead of result.
+        return L.copy()
+
+    # loop over values in L
     for i, l in enumerate(L):
-        subperm = permute(N-1, L.copy())
+
+        # get all permutations from lower levels
+        subperm = permute(N-1, L)
+
+        #loop over values in subperm
         for j in range(0,len(subperm)):
+
+            # append the current letter to the beginning of the subperm
             subperm[j] = l + subperm[j]
+
+        #add the subperm list to the result list
         result += subperm
+
+    # return overall result
     return result
+
 
 if len(sys.argv) != 3 or not is_int(sys.argv[1]) or not os.path.exists(sys.argv[2]):
     print("Error: format: permutations.py [WORD LENGTH] [INPUT_CHARACTERS.txt]")
-    exit()
+    exit(1)
 input_letters_filepath = sys.argv[2]
-print("input file:", input_letters_filepath, end="\t\n")
-input_letters = []
 word_length = int(sys.argv[1])
-print("word length:", word_length, end="\t\n")
+
+input_letters = []
+
+if word_length < 1:
+    print("Error: word length must be >= 1.")
+    exit(1)
+
+print("input file:\t{}".format(input_letters_filepath))
+print("word length:\t{}".format(word_length))
+
 with open(input_letters_filepath) as input_letters_file:
     print("\nreading from file...")
+
     input_letters = list(map(lambda s: s.strip(), input_letters_file.readlines()))
+
     print("read letters:", end="\t")
     print(*input_letters)
+
     for letter in input_letters:
         if len(letter) != 1 or not letter.isalpha():
             print("Error: input file must contain lines with single letters only")
-            exit()
+            exit(1)
+
     output_file = open("permutations.csv", "w")
+
     for word in permute(word_length, input_letters):
         output_file.write(word + ",\n")
-print("done! output: permutations.csv") 
+
+print()
+print("done! \noutput:\t\tpermutations.csv") 
